ddeadeee5067106251fe0848cc68f3eb56c45b92
thanosch94 <chatzopoulosthanos@gmail.com>
2024-05-12 01:20:10 +0300

    Send user to client on login

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -6,30 +6,41 @@ using Microsoft.EntityFrameworkCore;
 
 namespace DataNexApi.Controllers
 {
-    public class LoginController:BaseController
+    public class AccountController:BaseController
     {
         private ApplicationDbContext _context;
         private IMapper _mapper;
-        public LoginController(ApplicationDbContext context, IMapper mapper)
+        public AccountController(ApplicationDbContext context, IMapper mapper)
         {
             _context = context;
             _mapper = mapper;
         }
 
         [HttpPost("login")]
-        public async Task<IActionResult> GetAll([FromBody]LoginDto dto)
+        public async Task<IActionResult> Login([FromBody]LoginDto dto)
         {
+            var apiResponse = new ApiResponseDto();
+
             var success = false;
             var user = await _context.Users.Where(x => x.Username == dto.Username).FirstOrDefaultAsync();
             if (user != null && dto.Password != null)
             {
                 success = BCrypt.Net.BCrypt.EnhancedVerify(dto.Password, user.PasswordHash);
-            
             }
            
             if(success)
             {
-                return Ok(success);
+                var userToReturn = new UserDto()
+                {
+                    Id = user.Id,
+                    Name = user.Name,
+                    Email = user.Email,
+                    Username = user.Username,
+                    UserRole = user.UserRole,
+                };
+                apiResponse.Success = success;
+                apiResponse.Result = userToReturn;
+                return Ok(apiResponse);
 
             }
             else
@@ -38,5 +49,7 @@ namespace DataNexApi.Controllers
             }
 
         }
+
+      
     }
 }
