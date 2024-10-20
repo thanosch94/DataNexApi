f7bbc467a689ffc607eeded73eb1d66d0030dc25
thanosch94 <chatzopoulosthanos@gmail.com>
2024-10-20 19:05:40 +0300

    Added lots and general options

---
+++ Controllers/GeneralOptionsController.cs
@@ -0,0 +1,132 @@
+﻿using AutoMapper;
+using DataNex.Data;
+using DataNex.Model.Dtos;
+using DataNex.Model.Enums;
+using DataNex.Model.Models;
+using DataNexApi.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+
+namespace DataNexApi.Controllers
+{
+    public class GeneralOptionsController : BaseController
+    {
+        private ApplicationDbContext _context;
+        private IMapper _mapper;
+        private static readonly object _lockObject = new object();
+
+        public GeneralOptionsController(ApplicationDbContext context, IMapper mapper) : base(context)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        [HttpGet("getall")]
+        public async Task<IActionResult> GetAll()
+        {
+            Guid companyId = GetCompanyFromHeader();
+
+            var data = await _context.GeneralAppOptions.Where(x => x.CompanyId == companyId).FirstOrDefaultAsync();
+
+            return Ok(data);
+        }
+
+        [HttpPost("insertdto")]
+        public async Task<IActionResult> InsertDto([FromBody] GeneralOptionsDto dto)
+        {
+            Guid companyId = GetCompanyFromHeader();
+
+            var actionUser = await GetActionUser();
+
+            var data = new GeneralOptions();
+
+            var exists = await _context.GeneralAppOptions.Where(x=>x.CompanyId == companyId).FirstOrDefaultAsync();
+            if (exists == null)
+            {
+                data.LotsEnabled = dto.LotsEnabled;
+                data.UserAdded = actionUser.Id;
+                data.CompanyId = companyId;
+
+                lock (_lockObject)
+                {
+                    var maxNumber = _context.GeneralAppOptions.Where(x => x.CompanyId == companyId).Max(x => (x.SerialNumber)) ?? 0;
+                    data.SerialNumber = maxNumber + 1;
+                    data.Code = data.SerialNumber.ToString().PadLeft(5, '0');
+
+                    try
+                    {
+                        _context.GeneralAppOptions.Add(data);
+                        _context.SaveChanges();
+                        LogService.CreateLog($"General Options inserted by \"{actionUser.UserName}\". General Options: {JsonConvert.SerializeObject(data)}", LogTypeEnum.Information, LogOriginEnum.DataNexApp, actionUser.Id, _context);
+
+                    }
+                    catch (Exception ex)
+                    {
+                        LogService.CreateLog($"General Options could not be inserted by \"{actionUser.UserName}\". General Options: {JsonConvert.SerializeObject(data)} Error: {ex.Message}", LogTypeEnum.Error, LogOriginEnum.DataNexApp, actionUser.Id, _context);
+                        throw;
+                    }
+                };
+                return Ok(data);
+            }
+            else
+            {
+                return BadRequest("General Options already exist for this company");
+            }
+
+        }
+
+        [HttpPut("updatedto")]
+        public async Task<IActionResult> UpdateDto([FromBody] GeneralOptionsDto dto)
+        {
+            Guid companyId = GetCompanyFromHeader();
+
+            var actionUser = await GetActionUser();
+
+            var data = await _context.GeneralAppOptions.Where(x => x.Id == dto.Id && x.CompanyId == companyId).FirstOrDefaultAsync();
+
+            data.LotsEnabled = dto.LotsEnabled;
+            data.CompanyId = companyId;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+                LogService.CreateLog($"General Options updated by \"{actionUser.UserName}\". General Options: {JsonConvert.SerializeObject(data)}", LogTypeEnum.Information, LogOriginEnum.DataNexApp, actionUser.Id, _context);
+
+            }
+            catch (Exception ex)
+            {
+                LogService.CreateLog($"General Options could not be updated by \"{actionUser.UserName}\". General Options: {JsonConvert.SerializeObject(data)} Error: {ex.Message}", LogTypeEnum.Error, LogOriginEnum.DataNexApp, actionUser.Id, _context);
+
+            }
+
+            return Ok(data);
+
+        }
+
+        [HttpDelete("deletebyid/{id}")]
+        public async Task<IActionResult> DeleteById(Guid id)
+        {
+            Guid companyId = GetCompanyFromHeader();
+
+            var actionUser = await GetActionUser();
+
+            var data = await _context.GeneralAppOptions.FirstOrDefaultAsync(x => x.Id == id && x.CompanyId == companyId);
+
+            try
+            {
+                _context.GeneralAppOptions.Remove(data);
+
+                await _context.SaveChangesAsync();
+                LogService.CreateLog($"General Options deleted by \"{actionUser.UserName}\". General Options: {JsonConvert.SerializeObject(data)}", LogTypeEnum.Information, LogOriginEnum.DataNexApp, actionUser.Id, _context);
+
+            }
+            catch (Exception ex)
+            {
+                LogService.CreateLog($"General Options could not be deleted by \"{actionUser.UserName}\". General Options: {JsonConvert.SerializeObject(data)} Error: {ex.Message}", LogTypeEnum.Error, LogOriginEnum.DataNexApp, actionUser.Id, _context);
+            }
+            return Ok(data);
+        }
+    }
+
+}
---
+++ Controllers/LotsController.cs
@@ -0,0 +1,143 @@
+﻿using AutoMapper;
+using DataNex.Data;
+using DataNex.Model.Dtos;
+using DataNex.Model.Enums;
+using DataNex.Model.Models;
+using DataNexApi.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+
+namespace DataNexApi.Controllers
+{
+    [Authorize]
+    public class LotsController : BaseController
+    {
+        private ApplicationDbContext _context;
+        private IMapper _mapper;
+        private static readonly object _lockObject = new object();
+
+        public LotsController(ApplicationDbContext context, IMapper mapper) : base(context)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+
+        [HttpGet("getall")]
+        public async Task<IActionResult> GetAll()
+        {
+            Guid companyId = GetCompanyFromHeader();
+
+            var data = await _context.Lots.Where(x => x.CompanyId == companyId).ToListAsync();
+
+            return Ok(data);
+        }
+
+        [HttpPost("insertdto")]
+        public async Task<IActionResult> InsertDto([FromBody] LotDto dto)
+        {
+            Guid companyId = GetCompanyFromHeader();
+
+            var actionUser = await GetActionUser();
+
+            var data = new Lot();
+
+            //TODO check if validation will be with the code and not the name
+            var exists = await _context.Statuses.Where(x => x.Name == dto.Name && x.CompanyId == companyId).FirstOrDefaultAsync();
+            if (exists == null)
+            {
+                data.Name = dto.Name;
+                data.Notes = dto.Notes;
+                data.ProdDate = dto.ProdDate;   
+                data.ExpDate = dto.ExpDate;
+                data.UserAdded = actionUser.Id;
+                data.CompanyId = companyId;
+
+                lock (_lockObject)
+                {
+                    var maxNumber = _context.Lots.Where(x => x.CompanyId == companyId).Max(x => (x.SerialNumber)) ?? 0;
+                    data.SerialNumber = maxNumber + 1;
+                    data.Code = data.SerialNumber.ToString().PadLeft(5, '0');
+
+                    try
+                    {
+                        _context.Lots.Add(data);
+                        _context.SaveChanges();
+                        LogService.CreateLog($"Lot \"{data.Name}\" inserted by \"{actionUser.UserName}\". Lot: {JsonConvert.SerializeObject(data)}", LogTypeEnum.Information, LogOriginEnum.DataNexApp, actionUser.Id, _context);
+
+                    }
+                    catch (Exception ex)
+                    {
+                        LogService.CreateLog($"Lot \"{data.Name}\" could not be inserted by \"{actionUser.UserName}\". Lot: {JsonConvert.SerializeObject(data)} Error: {ex.Message}", LogTypeEnum.Error, LogOriginEnum.DataNexApp, actionUser.Id, _context);
+                        throw;
+                    }
+                };
+                return Ok(data);
+            }
+            else
+            {
+                return BadRequest("Lot already exists");
+            }
+
+        }
+
+        [HttpPut("updatedto")]
+        public async Task<IActionResult> UpdateDto([FromBody] LotDto dto)
+        {
+            Guid companyId = GetCompanyFromHeader();
+
+            var actionUser = await GetActionUser();
+
+            var data = await _context.Lots.Where(x => x.Id == dto.Id && x.CompanyId == companyId).FirstOrDefaultAsync();
+
+            data.Name = dto.Name; 
+            data.Notes = dto.Notes;
+            data.ProdDate = dto.ProdDate;
+            data.ExpDate = dto.ExpDate;
+            data.UserAdded = actionUser.Id;
+            data.CompanyId = companyId;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+                LogService.CreateLog($"Lot \"{data.Name}\" updated by \"{actionUser.UserName}\". Lot: {JsonConvert.SerializeObject(data)}", LogTypeEnum.Information, LogOriginEnum.DataNexApp, actionUser.Id, _context);
+
+            }
+            catch (Exception ex)
+            {
+                LogService.CreateLog($"Lot \"{data.Name}\" could not be updated by \"{actionUser.UserName}\". Lot: {JsonConvert.SerializeObject(data)} Error: {ex.Message}", LogTypeEnum.Error, LogOriginEnum.DataNexApp, actionUser.Id, _context);
+
+            }
+
+            return Ok(data);
+
+        }
+
+        [HttpDelete("deletebyid/{id}")]
+        public async Task<IActionResult> DeleteById(Guid id)
+        {
+            Guid companyId = GetCompanyFromHeader();
+
+            var actionUser = await GetActionUser();
+
+            var data = await _context.Lots.FirstOrDefaultAsync(x => x.Id == id && x.CompanyId == companyId);
+
+            try
+            {
+                _context.Lots.Remove(data);
+
+                await _context.SaveChangesAsync();
+                LogService.CreateLog($"Lot \"{data.Name}\" deleted by \"{actionUser.UserName}\". Lot: {JsonConvert.SerializeObject(data)}", LogTypeEnum.Information, LogOriginEnum.DataNexApp, actionUser.Id, _context);
+
+            }
+            catch (Exception ex)
+            {
+                LogService.CreateLog($"Lot \"{data.Name}\" could not be deleted by \"{actionUser.UserName}\". Lot: {JsonConvert.SerializeObject(data)} Error: {ex.Message}", LogTypeEnum.Error, LogOriginEnum.DataNexApp, actionUser.Id, _context);
+            }
+            return Ok(data);
+        }
+    }
+    
+}
